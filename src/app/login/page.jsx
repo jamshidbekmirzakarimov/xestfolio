2af6cd86e38b584a39c56340041db407e97739fe
Jamshidbek Codes <jamshidbekmirzakarimov6@gmail.com.>
2024-01-19 11:23:07 +0500

    fixed code

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -20,7 +20,7 @@ const Login = () => {
   }
 
   const onSubmit = (data) => {
-    fetch("http://95.130.227.6:8000/user/login", {
+    fetch("http://192.168.0.121:8000/user/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
