c016441fefc99e9a5f2e10d4c8c5553ace170033
Jamshidbek Codes <jamshidbekmirzakarimov6@gmail.com.>
2024-01-19 12:03:50 +0500

    bug fixed with Xurshid aka

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -2,20 +2,22 @@
 import { useRouter } from "next/navigation";
 import { isExpired } from "react-jwt";
 const Admin = () => {
-  const token = localStorage.getItem("jwt");
-  const router = useRouter();
-  if (token) {
-    const isMyTokenExpired = isExpired(token);
-    if (isMyTokenExpired) {
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("jwt");
+    const router = useRouter();
+    if (token) {
+      const isMyTokenExpired = isExpired(token);
+      if (isMyTokenExpired) {
+        localStorage.removeItem("jwt");
+        router.push("/login");
+        return;
+      }
+    }
+    if (!token) {
       localStorage.removeItem("jwt");
       router.push("/login");
       return;
     }
-  }
-  if (!token) {
-    localStorage.removeItem("jwt");
-    router.push("/login");
-    return;
   }
 
   return <div>Admin</div>;
